07a63558368ea7af804fa2c11de9df38b64c6b07
Olga <olga.dibts@ya.ru>
2021-05-28 20:45:07 +0300

    смена темы цветовой сайта

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import {Main} from './components/Main/Main'
+import { useMainThemeContext } from './context/MainThemeContext';
 function App() {
+  const {mainTheme} = useMainThemeContext()
+
   return (
-    <div className="App">
+    <div className={`App ${mainTheme}`}>
       <Main />
     </div>
   );
--- my-app/src/components/Main/Main.jsx
+++ my-app/src/components/Main/Main.jsx
@@ -2,15 +2,7 @@ import { useMainThemeContext } from "../../context/MainThemeContext";
 import { TodoList } from "../TodoList/TodoList";
 
 export const Main = () => {
-  const {mainTheme, setMainTheme} = useMainThemeContext()
-  const changeTheme = () => {
-    if (mainTheme === 'light') {
-      setMainTheme('dark')
-    } else {
-      setMainTheme('light')
-    }
-    console.log(mainTheme);
-  }
+
   return (
     <>
     <button onClick={changeTheme}>Change theme</button>
